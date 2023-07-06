9d2b69309c4625fb00dbc8bb86d891ba91389220
tmdat2301 <dattranmanh36@gmail.com>
2023-07-06 23:46:30 +0700

    add Provider<>  wrap any nested components to access Redux store

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigation from './screen/navigation/AppNavigation';
-import {EventRegister} from 'react-native-event-listeners';
-// import theme from './config/theme';
-// import ThemeContext from './config/themeContext';
-import { ThemeProvider } from './ThemeContext';
-
+import {ThemeProvider} from './ThemeContext';
+import {Provider} from 'react-redux';
+import {store} from './screen/redux/store';
 const App = () => {
-  // const [mode, setMode] = useState(false);
-  // useEffect(() => {
-  //   let eventListener = EventRegister.addEventListener('changeTheme', data => {
-  //     setMode(data);
-  //   });
-  //   return () => {
-  //     EventRegister.removeAllListeners(eventListener);
-  //   };
-  // });
-  // const [theme, setTheme] = React.useState('light');
-
-  // const toggleTheme = () => {
-  //   const nextTheme = theme === 'light' ? 'dark' : 'light';
-  //   setTheme(nextTheme);
-  // };
   return (
-    <ThemeProvider>
-       <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
-    </ThemeProvider>
-     
+    <Provider store={store}>
+      <ThemeProvider>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
